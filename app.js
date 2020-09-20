97ab1ac094ebe1d4e6463c2fa5ecbb4e4aba0bc7
Gurcan Hamali <gurcanhamali@gmail.com>
2020-09-20 18:08:54 +1000

    62. Creating and Mounting Multiple Routers

--- app.js
+++ app.js
@@ -138,22 +138,18 @@ const deleteUser = (req, res) => {
 };
 
 // 3-) ROUTES
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
+tourRouter.route('/').get(getAllTours).post(createTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+app.use('/api/v1/tours', tourRouter);
 
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
+userRouter.route('/').get(getAllUsers).post(createUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
-app
-  .route('/api/v1/users/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+app.use('/api/v1/users', userRouter);
 
 // 4-) START THE SERVER
 const port = 3000;
